b9cdc92d510d393d73420c569a6ce93d87882f2c
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-10-15 03:25:09 +0300

    fixed 3: line too long

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,13 +5,10 @@ from .models import Post, Group
 
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
     posts = (
         Post
         .objects
-        .order_by('-pub_date')[:10]        
+        .order_by('-pub_date')[:10]
     )
     # В словаре context отправляем информацию в шаблон
     context = {
