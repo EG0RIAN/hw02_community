0b89ae3168d8b6774555247f25773c5da8c54424
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-10-15 03:21:47 +0300

    fixed 2: line too long

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,7 +8,11 @@ def index(request):
     # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
     # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = (
+        Post
+        .objects
+        .order_by('-pub_date')[:10]        
+    )
     # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
@@ -26,7 +30,7 @@ def group_posts(request, slug):
     # Метод .filter позволяет ограничить поиск по критериям.
     # Это аналог добавления
     # условия WHERE group_id = {group_id}
-    posts = list(
+    posts = (
         Post
         .objects
         .filter(group=group)
